c872d867bd504ea550738710c4d78ebdf5e2d0f4
Tobias Hilbig <web.tobias@hilbig-ffb.de>
2020-04-17 20:30:47 +0200

    improve performance

--- encrypt.py
+++ encrypt.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-
 from PIL import Image
 from Crypto.Cipher import AES
 from Crypto import Random
 from argparse import ArgumentParser
 
-def encrypt_image(image, key):
+key = Random.new().read(AES.key_size[0])
+mode = AES.MODE_ECB
+aes = AES.new(key, mode)
+
+def encrypt_image(imagepath, key):
+    image = Image.open(imagepath)
     image_array = image.tobytes()
+
     padding_length = AES.block_size - len(image_array) % AES.block_size
     image_array += bytes(padding_length * ".", "UTF-8") # Just an arbitrary padding byte
-
-    mode = AES.MODE_ECB
-    aes = AES.new(key, mode)
 
     encrypted_image = aes.encrypt(image_array)
     encrypted_image = encrypted_image[:-padding_length]
@@ -19,19 +21,13 @@ def encrypt_image(image, key):
     return Image.frombytes("RGB", image.size, encrypted_image, "raw", "RGB")
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Encrypt images with AES ECB/CBC")
+    parser = ArgumentParser(description="Encrypt images with AES ECB")
 
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-ecb", "--ecb", action="store_true")
-
-    parser.add_argument("input_file", help="Input file")
-    parser.add_argument("output_file", help="Output file")
+    parser.add_argument('-i','--input', nargs='+', help='<Required> List of input files', required=True)
+    parser.add_argument('-o','--output', nargs='+', help='<Required> List of output files', required=True)
+    
     args = parser.parse_args()
-
-    image = Image.open(args.input_file).convert('RGBA').convert('RGB') # The example Tux.png is in palette mode with transparency, and needs to be converted to RGBA first
-                                                                       # TODO: Find a better way to do this in a general way
-
-    key = Random.new().read(AES.key_size[0])
-
-    encrypt_image(image, key).save(args.output_file)
+    
+    for i in range(0, len(args.input)):
+        encrypt_image(args.input[i], key).save(args.output[i])
 
